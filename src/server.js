1e2b36045d1c6e18a969f1cab8be02f94145fc90
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-16 14:17:44 +1100

    Configure basic CORS settings

--- src/server.js
+++ src/server.js
@@ -22,3 +22,11 @@ app.use(
     },
   })
 );
+
+// Configure basic CORS settings
+const cors = require("cors");
+const corsOptions = {
+  origin: ["http://localhost:5000"],
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
