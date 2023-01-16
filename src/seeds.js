2e02bbb987d56ea8045b95d73f46e90722f50f7e
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-16 15:34:08 +1100

    Create Role seeds

--- src/seeds.js
+++ src/seeds.js
@@ -7,3 +7,20 @@ const { Post } = require("./models/Post");
 
 const dotenv = require("dotenv");
 dotenv.config();
+
+const roles = [
+  {
+    name: "regular",
+    description:
+      "A regular user can view, create and read data. They can edit and delete only their own data.",
+  },
+  {
+    name: "admin",
+    description:
+      "An admin user has full access permissions to do anything within this API.",
+  },
+  {
+    name: "banned",
+    description: "A banned user can read data, but cannot do anything else.",
+  },
+];
