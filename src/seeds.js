08db3a6509b8e4b70f658d66a35ad8050b440266
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-16 16:26:27 +1100

    Finish seed file functionality

--- src/seeds.js
+++ src/seeds.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const { databaseConnector } = require("./database");
 
 const { Role } = require("./models/Role");
 const { User } = require("./models/User");
@@ -24,3 +23,64 @@ const roles = [
     description: "A banned user can read data, but cannot do anything else.",
   },
 ];
+
+const users = [];
+
+const posts = [];
+
+// Configure database environment settings
+let databaseURL = "";
+switch (process.env.NODE_ENV.toLowerCase()) {
+  case "test":
+    databaseURL = "mongodb://localhost:27017/ExpressAPI-test";
+    break;
+  case "development":
+    databaseURL = "mongodb://localhost:27017/ExpressAPI-dev";
+    break;
+  case "production":
+    databaseURL = process.env.DATABASE_URL;
+    break;
+  default:
+    console.error(
+      "Incorrect JavaScript environment specified, database will not be connected"
+    );
+    break;
+}
+// Connect to database
+const { databaseConnector } = require("./database");
+databaseConnector(databaseURL)
+  .then(() => {
+    console.log("Database connected successfully!");
+  })
+  .catch((error) => {
+    console.log(`An error occurred connecting to the database:
+  ${error}`);
+  })
+  .then(async () => {
+    // Parse all collections from database object
+    if (process.env.WIPE == "true") {
+      const collections = await mongoose.connection.db
+        .listCollections()
+        .toArray();
+
+      collections
+        .map((collection) => collection.name)
+        .forEach(async (collectionName) => {
+          // Drop all collections from database
+          mongoose.connection.db.dropCollection(collectionName);
+        });
+      console.log("Database wiped");
+    }
+  })
+  .then(async () => {
+    // Add seeds to database
+    await Role.insertMany(roles);
+    console.log("Database seeded");
+  })
+  .then(() => {
+    // Disconnect from database
+    mongoose.connection.close();
+    console.log("Seed connection closed");
+  });
+
+// NODE_ENV=development WIPE=true node src/seeds.js
