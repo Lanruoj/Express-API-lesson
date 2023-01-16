ae4a128d2e5d1eb47341f06a7995486c48283110
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-16 14:16:11 +1100

    Configure dotenv, environment vars, express app and helmet

---
+++ src/server.js
@@ -0,0 +1,24 @@
+// Make the environment variables from .env available for use
+const dotenv = require("dotenv");
+dotenv.config();
+// Configure environment variables from .env
+const HOST = process.env.HOST || "localhost";
+const PORT = process.env.PORT || 5000;
+
+// Create app instance using Express
+const express = require("express");
+const app = express();
+
+// Configure basic Helmet settings on our app
+const helmet = require("helmet");
+app.use(helmet());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.contentSecurityPolicy());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+    },
+  })
+);
