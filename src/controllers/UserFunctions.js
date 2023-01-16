9d0d3d63873266230ad9a306d424fd525372a049
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-17 10:13:33 +1100

    Create UserFunctions.js and build encryption/decryption config and functions

---
+++ src/controllers/UserFunctions.js
@@ -0,0 +1,31 @@
+const { User } = require("../models/User");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Encryption/decryption configuration
+const crypto = require("crypto");
+const encAlgorithm = "aes-256-cbc";
+const encPrivateKey = crypto.scryptSync(process.env.ENC_KEY, "SpecialSalt", 32);
+const encIV = crypto.scryptSync(process.env.ENC_IV, "SpecialSalt, 16");
+let cipher = crypto.createCipheriv(encAlgorithm, encPrivateKey, encIV);
+let decipher = crypto.createDecipheriv(encAlgorithm, encPrivateKey, encIV);
+
+//// Encryption & decryption functionality
+
+// Encrypt a string
+function encryptString(data) {
+  cipher = crypto.createCipheriv(encAlgorithm, encPrivateKey, encIV);
+  return cipher.update(data, "utf8", "hex") + cipher.final("hex");
+}
+
+// Decrypt a string
+function decryptString(data) {
+  decipher = crypto.createDecipheriv(encAlgorithm, encPrivateKey, encIV);
+  return decipher.update(data, "hex", "utf8") + decipher.final("utf8");
+}
+
+// Decrypts a stringified JSON object and converts to regular object
+function decryptObject(data) {
+  return JSON.parse(decryptString(data));
+}
