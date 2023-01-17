de815af9296f269621728162751e4b3f5f5a6633
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-17 12:43:42 +1100

    Complete all CRUD routes for /posts

--- src/controllers/PostRoutes.js
+++ src/controllers/PostRoutes.js
@@ -22,4 +22,48 @@ router.get("/", async (request, response) => {
   });
 });
 
+// Get Post by provided ID
+// [GET] /posts/:postID
+router.get("/:postID", async (request, response) => {
+  const post = await getPostByID(request.params.postID);
+
+  return response.json(post);
+});
+
+// Get Post by author ID
+// [GET] /posts/:authorID
+router.get("/:authorID", async (request, response) => {
+  const post = await getPostsByAuthor(request.params.authorID);
+
+  return response.json(post);
+});
+
+// Create a new Post
+// [POST] /posts
+router.post("/", async (request, response) => {
+  const newPost = await createPost(request.body.postDetails);
+
+  return response.json();
+});
+
+// Update a Post
+// [PUT] /posts/:postID
+router.put("/:postID", async (request, response) => {
+  const postDetails = {
+    postID: request.params.postID,
+    updateData: request.body.updateData,
+  };
+  const updatedPost = await updatePost(postDetails);
+
+  return response.json(updatedPost);
+});
+
+// Delete a Post
+// [DELETE] /posts/:postID
+router.delete("/:postID", async (request, response) => {
+  const deletedPost = await deletePost(request.params.postID);
+
+  return response.json(deletedPost);
+});
+
 module.exports = router;
